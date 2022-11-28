ef9b1ff745311fd930885b6dae6c6aef32374c22
andreypats <andrey.it.work@gmail.com>
2022-11-29 01:04:20 +0300

    Lesson 2 is learning (2_26_00)

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import s from './Projects.module.css'
+import s from './Projects.module.scss'
 import styleContainer from '../../src/common/styles/Container.module.css'
 import {Project} from "./project/Project";
 import {Title} from "../common/components/title/Title";
+import todoImage from './../assets/image/todo.jpg'
+import socialImage from './../assets/image/social_media.jpg'
 
 export const Projects = () => {
+    const social = {
+        backgroundImage: 'url(' + socialImage + ')',
+    };
+
+    const todo = {
+        backgroundImage: 'url(' + todoImage + ')',
+    };
+
+    const descriptionSocial = 'Dolore officia sint incididunt non excepteur ea mollit commodo ut enim reprehenderit cupidatat labore ad laborum consectetur consequat...'
+
+    const descriptionTodo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi tempora perferendis vero officia enim impedit voluptatem dignissimos, veniam ratione est alias rerum aperiam, nam aliquam reprehenderit iste dolor.'
+
     return (
+
         <div className={s.projectsBlock}>
             <div className={`${styleContainer.container} ${s.projectsContainer}`   }>
                 <Title title={'Projects'}/>
                 <div className={s.projects}>
-                    <Project title={'Project 1'} description={'Dolore officia sint incididunt non excepteur ea mollit commodo ut enim reprehenderit cupidatat labore ad laborum consectetur consequat...'}/>
-                    <Project title={'Project 2'} description={'Dolore officia sint incididunt non excepteur ea mollit commodo ut enim reprehenderit cupidatat labore ad laborum consectetur consequat...'}/>
+                    <Project style={social} title={'Social Network'} description={descriptionSocial}/>
+                    <Project style={todo} title={'Todolist'} description={descriptionTodo}/>
                 </div>
             </div>
         </div>
--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import s from './Project.module.css'
+import s from './Project.module.scss'
 
 type ProjectsType = {
+    style: object
     title: string
     description: string
 }
@@ -9,7 +10,7 @@ type ProjectsType = {
 export const Project = (props: ProjectsType) => {
     return (
         <div className={s.project}>
-            <div className={s.image}>
+            <div className={s.image} style={props.style}>
                 <button className={s.button}>Watch</button>
             </div>
             <h4 className={s.projectTitle}>{props.title}</h4>
