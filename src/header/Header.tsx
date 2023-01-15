450a53835d11b624fdd04d94602f2d1d092c4d34
andreypats <andrey.it.work@gmail.com>
2023-01-16 00:24:07 +0300

    Add transparent style to Header

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './Header.module.scss'
 import {Nav} from "../nav/Nav";
 
 export const Header = () => {
+
+    const [headerShow, setHeaderShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+    }, [headerShow]);
+
+    const handleScroll = () =>{
+        (window.scrollY > 0) ? setHeaderShow(true) : setHeaderShow(false);
+    }
+
     return (
-        <div className={s.header}>
+        <div className={s.header} style={{
+            backgroundColor: headerShow ? "#212529" : "transparent"
+        }}>
             <Nav />
         </div>
     );
