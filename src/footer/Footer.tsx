7a396de1edb01be3172f837178abf457307b99ec
andreypats <andrey.it.work@gmail.com>
2023-03-09 12:16:21 +0300

    Change date in footer

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     return (
         <div className={s.footer}>
             <div className={s.footerContainer}>
-                <div className={s.copyrights}>&copy; 2022 All rights reserved - <span>Andrey Pats</span></div>
+                <div className={s.copyrights}>&copy; 2023 All rights reserved - <span>Andrey Pats</span></div>
             </div>
         </div>
     );
