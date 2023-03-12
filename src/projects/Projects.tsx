388d7eb03c3f952ee42cd5b4220483829d08ce7e
andreypats <andrey.it.work@gmail.com>
2023-03-13 01:08:08 +0300

    Add Chat project

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -3,33 +3,33 @@ import s from './Projects.module.scss'
 import {Project} from "./project/Project";
 import {Title} from "../common/components/title/Title";
 import cardsImage from '../assets/image/cards.png'
-import todoImage from '../assets/image/todo.jpg'
-import socialImage from '../assets/image/social_media.jpg'
+import todoImage from '../assets/image/todo.png'
+import chatImage from '../assets/image/chat.png'
 import {Fade} from "react-awesome-reveal";
 
 export const Projects = () => {
     const cards = {backgroundImage: 'url(' + cardsImage + ')'};
 
-    const social = {backgroundImage: 'url(' + socialImage + ')'};
+    const chat = {backgroundImage: 'url(' + chatImage + ')'};
 
     const todo = {backgroundImage: 'url(' + todoImage + ')'};
 
     const descriptionCards = 'A project to create training cards on arbitrary topics using typescript & react-redux. The project implements registration and login forms, parameter filtering and interaction with server data, the creation of a personal account, etc.'
 
-    const descriptionSocial = 'Dolore officia sint incididunt non excepteur ea mollit commodo ut enim reprehenderit cupidatat labore ad laborum consectetur consequat...'
+    const descriptionTodo = 'A project to create task lists using typescript, redux-toolkit, material-ui. The project implements the ability to create (delete) task lists, edit and sort them.'
 
-    const descriptionTodo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi tempora perferendis vero officia enim impedit voluptatem dignissimos, veniam ratione est alias rerum aperiam, nam aliquam reprehenderit iste dolor.'
+    const descriptionChat = 'Creating a messenger for message exchange'
 
     const imageName = (name: string) => {
         if (name === 'cards') {
             // window.location.href='https://cards-nikskozlov.vercel.app/'
             window.open('https://cards-nikskozlov.vercel.app/');
         }
-        if (name === 'social') {
-            window.open('https://andreypats.github.io/portfolio/');
-        }
         if (name === 'todo') {
-            window.open('https://andreypats.github.io/portfolio/');
+            window.open('https://andreypats.github.io/it-incubator-todolist-ts-01/');
+        }
+        if (name === 'chat') {
+            window.open('https://andreypats.github.io/chat/');
         }
     }
 
@@ -40,8 +40,8 @@ export const Projects = () => {
                     <Title title={'Projects'}/>
                     <div className={s.projects}>
                         <Project id={'cards'} style={cards} title={'Learn cards'} description={descriptionCards} imageName={imageName}/>
-                        <Project id={'social'} style={social} title={'Social Network'} description={descriptionSocial} imageName={imageName}/>
                         <Project id={'todo'} style={todo} title={'Todolist'} description={descriptionTodo} imageName={imageName}/>
+                        <Project id={'chat'} style={chat} title={'Chat'} description={descriptionChat} imageName={imageName}/>
                     </div>
                 </div>
             </Fade>
